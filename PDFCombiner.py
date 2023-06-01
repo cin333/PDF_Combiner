993eb0061a436cbee53e973e5e95b431d1be4a11
cin333 <97006153+cin333@users.noreply.github.com>
2023-06-01 21:20:34 +1000

    Add files via upload

---
+++ PDFCombiner.py
@@ -0,0 +1,85 @@
+import pandas as pd  # useful parsing module
+import os
+import fitz
+import PySimpleGUI as sg
+
+# Reference: https://stackoverflow.com/questions/63725995/how-to-display-files-in-folder-when-using-pysimplegui-filebrowse-function
+
+def PDF_combiner(foldername, name):
+    # Change both for your own computer directory
+    directory = foldername
+
+    result = fitz.open()
+
+    for pdf in os.listdir(directory):
+        with fitz.open(directory + "\\" + pdf) as mfile:
+            result.insert_pdf(mfile)
+
+    result.save(directory + "\\" + name + ".pdf")
+
+    return 
+
+sg.theme("NeutralBlue")
+
+layout_1 = [
+    [sg.Input(key="Folder_Directory"), sg.Button('Browse Folders', key="FolderBrowse")],
+
+    [sg.Text('Files')],
+    [sg.Multiline(key='files', size=(60,20), autoscroll=True)],
+    
+    [sg.Text("Combine These Folders?"), sg.Button("Yes"), sg.Button("No")],
+
+    [sg.Exit()],    
+]
+
+layout_2 = [
+    [sg.Text("Enter Combined PDF Filename: "), sg.Input("Folder Name", do_not_clear=True), sg.Button("Enter", key="-ENTER-")], 
+    [sg.Button("Back", key="-BACK-")]
+]
+
+
+layout = [
+    [sg.Column(layout_1, key = '-COL1-'), sg.Column(layout_2, visible = False, key = '-COL2-')], 
+          ]
+
+window = sg.Window('Test', layout)
+
+
+layout = 1 # the currently visible layout
+while True:
+    event, values = window.read()
+    filenames = "" # Empty for now
+    print(event, values)
+     
+    if event is None or event == 'Exit' or event == 'Exit0':
+        window.close()
+        break
+
+    elif event == 'FolderBrowse':
+        foldername = sg.PopupGetFolder('Select Folder', no_window=True)
+        print(foldername)
+        if foldername: # `None` when clicked `Cancel` - so I skip it
+            filenames = sorted(os.listdir(foldername))
+            # it use `key='files'` to `Multiline` widget
+            window['files'].update("\n".join(filenames))
+        window["Folder_Directory"].update(foldername)
+    
+    elif event == 'Yes':
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = int(2)
+        window[f'-COL{layout}-'].update(visible=True)
+    
+    elif event == "-ENTER-":
+        PDF_combiner(foldername, values[1])
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = int(1)
+        window[f'-COL{layout}-'].update(visible=True)
+        values[0] = 0
+    
+    elif event == "-BACK-":
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = int(1)
+        window[f'-COL{layout}-'].update(visible=True)
+        
+            
+
